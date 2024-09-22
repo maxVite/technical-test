78fda0e47616f34e1ec0afcb8193e4715931e525
maxVite <maxi.vitelli.es@gmail.com>
2024-09-22 15:07:30 +0200

    feat: enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3001);
 }
 
